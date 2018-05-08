dfaec603b011baedfd9e7f4c4d3e4031c734eba9
HankeyDesigns <baeschtigst-register@yahoo.de>
2018-05-08 12:18:29 +0200

    Grüße zurück

--- app/src/main/java/com/example/odayk/mensaapp/Graph.java
+++ app/src/main/java/com/example/odayk/mensaapp/Graph.java
@@ -1,7 +1,7 @@
 package com.example.odayk.mensaapp;
 
 
-import android.graphics.Color; // Hallo Martin
+import android.graphics.Color; // Hallo Martin, Hallo Oday
 import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
